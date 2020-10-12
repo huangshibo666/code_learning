801a5322f59e77035e53500a5a3a3deff03bd6f8
huangshibo <huangshibo@58.com>
2020-10-12 20:07:40 +0800

    redis工具类初始化

---
+++ src/main/scala/com/huang/codelearning/scala/utils/Utils.scala
@@ -0,0 +1,14 @@
+package com.huang.codelearning.scala.utils
+
+/**
+ * @Description:
+ * @Author: huangshibo
+ * @Date: 2020/10/12 19:30
+ */
+object Utils {
+
+  def test() : Unit = {
+    println("scala utils")
+  }
+
+}
