e6be796632f308535b4e7c7665fed0dc814784d4
huangshibo <pavers1023@163.com>
2020-12-19 14:59:21 +0800

    1.测试

--- src/main/java/com/huang/codelearning/java/utils/Utils.java
+++ src/main/java/com/huang/codelearning/java/utils/Utils.java
@@ -5,7 +5,10 @@ package com.huang.codelearning.java.utils;
  * java utils
  */
 public class Utils {
-
+    /**
+     *
+     * @return void
+     */
     public static void test(){
         System.out.println("java utils");
     }
